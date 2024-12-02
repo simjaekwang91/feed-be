b02289fce904e1ee626dae7bf57fea3cd0ee5bbf
jasim <jasim@gmarket.com>
2024-12-02 22:31:20 +0900

    [fix] entity table override 추가

--- src/main/kotlin/com/liner/backend/entity/PageInfo.kt
+++ src/main/kotlin/com/liner/backend/entity/PageInfo.kt
@@ -7,6 +7,7 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
 
 @Entity
 @Table(name = "page_info")
@@ -14,9 +15,17 @@ class PageInfo (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "page_id")
-    val pageId: Long? = null,
+    val pageId: Long = 0L,
     @Column(name = "user_id")
     var userId: String = "",
     @Embedded
     var auditInfo: AuditInfo = AuditInfo(),
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is PageInfo) return false
+        return pageId == other.pageId
+    }
+
+    override fun hashCode(): Int = pageId.hashCode()
+}
