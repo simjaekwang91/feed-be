31b632759a1c6cebe122abdd67025464de47414c
jasim <jasim@gmarket.com>
2024-12-04 08:21:20 +0900

    [fix] businessexception type 추가, 코드 유효성 검사 추가

--- src/main/kotlin/com/liner/backend/exception/BusinessRuleException.kt
+++ src/main/kotlin/com/liner/backend/exception/BusinessRuleException.kt
@@ -6,5 +6,6 @@ class BusinessRuleException(
 
 enum class BusinessErrorType(val errorMessage: String, val code: Int) {
     UNKNOWN("정의되지 않은 에러입니다.",9999),
-    NOT_FOUND_USER("찾을 수 없는 사용자 입니다.", 9998)
+    NOT_BLANK_USER_ID("사용자 id는 빈 값일 수 없습니다.", 9998),
+    NOT_EMPTY_PAGE_LIST("페이지 리스트가 비어 있을 수 없습니다.", 8888)
 }
--- src/main/kotlin/com/liner/backend/service/FeedService.kt
+++ src/main/kotlin/com/liner/backend/service/FeedService.kt
@@ -1,5 +1,7 @@
 package com.liner.backend.service
 
+import com.liner.backend.exception.BusinessErrorType
+import com.liner.backend.exception.BusinessRuleException
 import com.liner.backend.model.response.HighlightDto
 import com.liner.backend.model.response.PageListDto
 import com.liner.backend.repository.HighlightRepository
@@ -13,7 +15,11 @@ class FeedService(
     private val highlightRepository: HighlightRepository
 ) {
     fun getFeedList(userId: String, pageNo: Long, pageSize: Long): List<PageListDto>? {
-         pageInfoRepository.getUserPageList(userId, pageSize, pageNo).also { pageInfoList ->
+        if (userId.isBlank()) throw BusinessRuleException(BusinessErrorType.NOT_BLANK_USER_ID)
+
+        pageInfoRepository.getUserPageList(userId, pageSize, pageNo).also { pageInfoList ->
+            if(pageInfoList.isEmpty()) throw BusinessRuleException(BusinessErrorType.NOT_EMPTY_PAGE_LIST)
+
             //검색된 페이지 정보의 id를 모아서 한번에 in절로 highlight 검색
             val pageIdList = pageInfoList.map { it.pageId }
             val highlights = highlightRepository.findTop3HighlightsForPageIdList(pageIdList)
